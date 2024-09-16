10cd0d6f1dca487e2d1f6577865b1aff1e26cde8
Gia Huy <trandinhgiahuy2004@gmail.com>
2024-09-16 16:20:28 +0700

    update chatroom

--- src/Core/Clinic.Domain/Commons/Entities/ChatRoom.cs
+++ src/Core/Clinic.Domain/Commons/Entities/ChatRoom.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Clinic.Domain.Commons.Entities.Base;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Clinic.Domain.Commons.Entities
+namespace Clinic.Domain.Commons.Entities;
+
+/// <summary>
+///     Represent the "ChatRoom" table.
+/// </summary>
+public class ChatRoom : IBaseEntity, ICreatedEntity, ITemporarilyRemovedEntity, IUpdatedEntity
 {
-    internal class ChatRoom
-    {
-    }
+    // Primary keys.
+    public Guid Id { get; set; }
+
+    // Foreign keys.
+    public Guid PatientId { get; set; }
+
+    public Guid DoctorId { get; set; }
+
+    // Normal columns.
+    public DateTime CreatedAt { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public Guid UpdatedBy { get; set; }
+
+    public DateTime RemovedAt { get; set; }
+
+    public Guid RemovedBy { get; set; }
+
+
+    // Navigation properties.
+    public IEnumerable<ChatContent> ChatContents { get; set; }
+    public QueueRoom QueueRoom { get; set; }
+    public Patient Patient { get; set; }
+    public Doctor Doctor { get; set; }  
 }
