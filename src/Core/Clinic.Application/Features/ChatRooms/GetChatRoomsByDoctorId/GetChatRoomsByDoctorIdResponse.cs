ae7c03580af5b7b8c40ae044111acf2b2ace5523
ngdatdev <nvdatdz8b@gmail.com>
2024-10-19 18:17:14 +0700

    Fix_Stuff

--- src/Core/Clinic.Application/Features/ChatRooms/GetChatRoomsByDoctorId/GetChatRoomsByDoctorIdResponse.cs
+++ src/Core/Clinic.Application/Features/ChatRooms/GetChatRoomsByDoctorId/GetChatRoomsByDoctorIdResponse.cs
@@ -24,6 +24,7 @@ public sealed class GetChatRoomsByDoctorIdResponse : IFeatureResponse
             public Guid ChatRoomId { get; init; }
             public string FullName { get; init; }
             public string Avatar { get; init; }
+            public string Title { get; init; }
             public bool IsEndConversation { get; init; }
         }
     }
