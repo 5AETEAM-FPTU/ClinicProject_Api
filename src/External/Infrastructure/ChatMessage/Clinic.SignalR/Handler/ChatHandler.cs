671a422fae262c0623b4bb5b683195df969f0a18
From_VuVuDeveloper <vuvo070403@gmail.com>
2024-10-20 17:55:45 +0700

    Chat

--- src/External/Infrastructure/ChatMessage/Clinic.SignalR/Handler/ChatHandler.cs
+++ src/External/Infrastructure/ChatMessage/Clinic.SignalR/Handler/ChatHandler.cs
@@ -23,16 +23,16 @@ public class ChatHandler : IChatHandler
 
     public async Task<bool> SendMessageAsync(ChatMessage chatMessage)
     {
-        var isChatRoomExperid =
-            await _unitOfWork.CreateChatContentRepository.IsChatRoomExperiedQueryAsync(
-                chatRoomId: Guid.Parse(input: chatMessage.ChatRoomId),
-                cancellationToken: default
-            );
+        //var isChatRoomExperid =
+        //    await _unitOfWork.CreateChatContentRepository.IsChatRoomExperiedQueryAsync(
+        //        chatRoomId: Guid.Parse(input: chatMessage.ChatRoomId),
+        //        cancellationToken: default
+        //    );
 
-        if (isChatRoomExperid)
-        {
-            return false;
-        }
+        //if (isChatRoomExperid)
+        //{
+        //    return false;
+        //}
 
         var chatContentId = Guid.NewGuid();
         var chatContent = new ChatContent()
@@ -45,13 +45,13 @@ public class ChatHandler : IChatHandler
                     Id = Guid.NewGuid(),
                     FilePath = asset,
                     FileName = asset,
-                    Type = "image"
+                    Type = "image",
                 })
                 .ToList(),
             CreatedAt = DateTime.Now,
             CreatedBy = Guid.Parse(input: chatMessage.SenderId),
             SenderId = Guid.Parse(input: chatMessage.SenderId),
-            ChatRoomId = Guid.Parse(input: chatMessage.ChatRoomId)
+            ChatRoomId = Guid.Parse(input: chatMessage.ChatRoomId),
         };
 
         var dbResult = await _unitOfWork.CreateChatContentRepository.AddChatContentCommandAsync(
