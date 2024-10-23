722a6d35a46c75654da312ed4b049722360328bc
ngdatdev <nvdatdz8b@gmail.com>
2024-10-24 01:20:22 +0700

    Fix stuff

--- src/External/Infrastructure/Persistence/Relational/Clinic.MySQL/Repositories/Auths/Login/LoginRepository.cs
+++ src/External/Infrastructure/Persistence/Relational/Clinic.MySQL/Repositories/Auths/Login/LoginRepository.cs
@@ -71,7 +71,7 @@ public class LoginRepository : ILoginRepository
     {
         return _users.AnyAsync(
             predicate: user =>
-                user.Id != userId
+                user.Id == userId
                 && user.RemovedBy != CommonConstant.DEFAULT_ENTITY_ID_AS_GUID
                 && user.RemovedAt != CommonConstant.MIN_DATE_TIME,
             cancellationToken: cancellationToken
