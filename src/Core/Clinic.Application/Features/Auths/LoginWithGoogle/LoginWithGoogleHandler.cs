99e049e9046e1b85049272aac03df1a86d2eba24
ngdatdev <nvdatdz8b@gmail.com>
2024-11-12 22:24:33 +0700

    Oke

--- src/Core/Clinic.Application/Features/Auths/LoginWithGoogle/LoginWithGoogleHandler.cs
+++ src/Core/Clinic.Application/Features/Auths/LoginWithGoogle/LoginWithGoogleHandler.cs
@@ -94,7 +94,7 @@ internal sealed class LoginWithGoogleHandler
             // Create user command.
             var dbResult = await _unitOfWork.LoginWithGoogleRepository.CreateUserCommandAsync(
                 user: newUser,
-                defaultPassword: _configuration["Authentication:DefaultPassword"],
+                defaultPassword: newUser.Email,
                 cancellationToken: ct
             );
 
